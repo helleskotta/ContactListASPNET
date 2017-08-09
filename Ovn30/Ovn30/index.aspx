a1ccfc9294912de4d63688da28c97a2e01f71669
Helen Luu <helleskotta@gmail.com>
2017-08-09 08:32:50 +0200

    Lade till allt

---
+++ Ovn30/Ovn30/index.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Ovn30.index" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
+    <h1>Yet another contactlist</h1>
+    <div id="left">
+        <asp:Label ID="FirstNameLabel" runat="server" Text="First name" Width="100px"></asp:Label>
+        <asp:TextBox ID="FirstNameInput" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="LastNameLabel" runat="server" Text="Last name" Width="100px"></asp:Label>
+        <asp:TextBox ID="LastNameInput" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="SSNLabel" runat="server" Text="SSN" Width="100px"></asp:Label>
+        <asp:TextBox ID="SSNInput" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="space" runat="server" Text="" Width="100px"></asp:Label>
+        <asp:Button ID="AddAContact" runat="server" Text="Add" OnClick="AddAContact_Click" />
+        <!-- TO DO Alert - OBS! Vill du verkligen lägga till samma kontakt igen? Ja/Nej/Uppdatera befintlig-->
+        <asp:Button ID="UpdateContactButton" runat="server" Text="Update" />
+        <asp:Button ID="DeleteContactButton" runat="server" Text="Delete" OnClick="DeleteContactButton_Click" />
+
+    </div>
+
+    <div id="right"> 
+        <asp:ListBox ID="ListboxContacts" runat="server" Height="429px" Width="333px" AutoPostBack="True" OnSelectedIndexChanged="ListboxContacts_SelectedIndexChanged"></asp:ListBox>
+    </div>
+
+</asp:Content>
---
+++ Ovn30/Ovn30/index.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SqlLib;
+
+namespace Ovn30
+{
+    public partial class index : System.Web.UI.Page
+
+    {
+        List<Contact> contacts = new List<Contact>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            UpdateListbox();
+        }
+
+        private void UpdateListbox()
+        {
+            List<Contact> contacts = MySQL.ReadContacts();
+
+            ListboxContacts.Items.Clear();
+
+            foreach (var contact in contacts)
+            {
+                ListboxContacts.Items.Add(new ListItem($"{contact.FirstName} {contact.LastName}", contact.ID.ToString()));
+            }
+        }
+
+
+        protected void AddAContact_Click(object sender, EventArgs e)
+        {
+            string firstName = FirstNameInput.Text;
+            string lastName = LastNameInput.Text;
+            string ssn = SSNInput.Text;
+            
+
+            if (firstName.Length <= 2 || lastName.Length <= 2 || ssn.Length != 10)
+            {
+                // Felmeddelande
+            }
+            else
+            {
+                MySQL.CreateContact(firstName, lastName, ssn);
+                FirstNameInput.Text = "";
+                LastNameInput.Text = "";
+                SSNInput.Text = "";
+
+                UpdateListbox();
+            }
+        }
+
+        protected void ListboxContacts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int contactID = Convert.ToInt32(ListboxContacts.SelectedValue);
+            var tempContact = MySQL.GetContactByID(contactID);
+
+            FirstNameInput.Text = tempContact.FirstName;
+            LastNameInput.Text = tempContact.LastName;
+            SSNInput.Text = tempContact.SSN;
+        }
+
+        protected void DeleteContactButton_Click(object sender, EventArgs e)
+        {
+            int contactID = Convert.ToInt32(ListboxContacts.SelectedValue);
+            FirstNameInput.Text = "";
+            LastNameInput.Text = "";
+            SSNInput.Text = "";
+            MySQL.DeleteContact(contactID);
+
+            UpdateListbox();
+
+        }
+    }
+}
